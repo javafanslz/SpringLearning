e760769c5b96e847f7fc38dba64e61766915b390
lizhu <lizhu@channelsoft.com>
2018-03-08 09:56:02 +0800

    lizhu分支

---
+++ src/main/java/com/java/thread/ThreadTest.java
@@ -0,0 +1,18 @@
+package com.java.thread;
+
+/**
+ * <dl>
+ * <dt> ThreadTest</dt>
+ * <dd>Description:</dd>
+ * <dd>Copyright: Copyright (C) 2016</dd>
+ * <dd>Company: 青牛（北京）技术有限公司</dd>
+ * <dd>CreateDate:2018/2/28</dd>
+ * </dl>
+ *
+ * @author lizhu
+ */
+public class ThreadTest {
+    public static void main(String[] args){
+        Thread thread = new Thread();
+    }
+}
